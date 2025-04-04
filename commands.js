3eafe9c7d188bace3f70575e47348be18e4dd7ef
FenilAhir98980 <backupsystem9909@gmail.com>
2025-04-04 15:25:37 -0700

    Added a Custom Joke Command

---
+++ commands.js
@@ -0,0 +1,24 @@
+import { version } from "mongoose"
+import { REST, Routes } from 'discord.js';
+import 'dotenv/config'
+const token = process.env.DISCORD_TOKEN
+const clientid = process.env.CLIENT_ID
+const commands = [
+    {
+        name:'joke',
+        description:'Replies the user with an joke from an custom API',
+    },
+];
+
+const rest = new REST({version: '10'}).setToken(token)
+
+try {
+    console.log('Started refreshing application (/) commands.');
+  
+    await rest.put(Routes.applicationCommands(clientid), { body: commands });
+  
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+  // this code would help us to register our commands onto our server.
--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-import { configDotenv } from "dotenv";
+import 'dotenv/config'
+import { Client,Events,GatewayIntentBits } from "discord.js";
+const token =  process.env.DISCORD_TOKEN
+import axios from 'axios';
+// Importing essentials from discord.js.
+// First import step is to setup our client that would login into discord and helping us to interact with our server on discord
+const client = new Client({intents:[GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent]})
+// Intent meaning what is that we want our bot to do.
+// using the client to listen the messages and respond on them
+client.on('messageCreate', (messgaevalue) => {
+    if(messgaevalue.author.bot) return 
+})
 
-const token = process.env.discordtoken
-console.log(token)
+ client.on('interactionCreate', async (interaction) => {
+    if(!interaction.isCommand) return;
+    // This line helps to ensure that the bot only interacts to the slash command not other events like button presses and menu selection.
+     
+     if(interaction.commandName === 'joke'){
+      const response = await axios.get('https://icanhazdadjoke.com/',{
+        headers :{Accept : 'application/json'}
+        // This line would make sure that we receive the data in json format meaning that our bot wants the data
+        // in json format.
+      })
+      // Axios is helping us to make an get request to the joke api to fetch data from it.
+      //
+      await interaction.reply(response.data.joke)
+      // Finally getting the joke from the response and telling our bot to give the user.
+     }
+
+ })
+
+client.login(token)
+ 
